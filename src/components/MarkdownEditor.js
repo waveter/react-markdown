66496f90324a06a5aa1822901317187bbd705010
waveter <mrnguyenbathuy@outlook.com>
2020-11-01 16:42:28 +0700

    Customize toolbar

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,17 +1,17 @@
 import React from "react";
-import "./MarkdownEditor.css";
-import AceEditor from "react-ace";
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-github";
-import "brace/ext/language_tools";
-import "ace-builds/src-noconflict/ext-language_tools";
-import { addCompleter } from "ace-builds/src-noconflict/ext-language_tools";
-import jsBeautify from "js-beautify";
-import { Button } from "react-bootstrap";
-import { LIST_COMMON_TOKENS, LIST_SUGGESTION_KEY } from "../constant/const";
 import ReactMarkdown from "react-markdown";
 import ReactMde from "react-mde";
 import "react-mde/lib/styles/css/react-mde-all.css";
+import "./MarkdownEditor.css";
+import { FaImage } from "react-icons/fa";
+
+const uploadImageCommand = {
+  name: "upload-image-command",
+  icon: () => <FaImage className="upload-image" />,
+  execute: opts => {
+    opts.textApi.replaceSelection("TBD");
+  }
+};
 
 class MarkdownEditor extends React.Component {
   constructor() {
@@ -32,7 +32,18 @@ class MarkdownEditor extends React.Component {
     return (
       <div className="body-container">
         <div className="markdown-editor">
-          <ReactMde value={this.state.input} onChange={this.setValue} />
+          <ReactMde
+            commands={{
+              "upload-image": uploadImageCommand
+            }}
+            toolbarCommands={[
+              ["header", "bold", "italic", "strikethrough"],
+              ["link", "quote", "code", "image", "upload-image"],
+              ["unordered-list", "ordered-list", "checked-list"]
+            ]}
+            value={this.state.input}
+            onChange={this.setValue}
+          />
         </div>
         <div className="markdown-preview">
           <ReactMarkdown source={this.state.input} />
